cf6db1a24bbd021d338196a96fa5273b93e21dd6
chema <chema@chema.com>
2020-11-07 07:08:28 -0600

    join to socket

--- socket_connecotr.py
+++ socket_connecotr.py
@@ -5,6 +5,12 @@ sio = socketio.Client()
 @sio.event
 def connect():
     print('connection established')
+    # Get ID
+    # TODO esta mac address es de Chema, comenatar para tomar la real 
+    mac = ['dc:fb:48:29:94:d7']
+    # mac = bluetooth.read_local_bdaddr()
+    my_id = int(mac[-1].replace(':', ''), 16)
+    sio.emit('joinAgent',{'curp': my_id,'sala':0})
 
 @sio.event
 def my_message(data):
